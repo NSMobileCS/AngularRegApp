51483a9842eb790a04282df3731be76ace2c09e0
Nathan R Smith <nrsmith012@gmail.com>
2017-10-16 14:01:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = {
+    first_name: '', 
+    last_name: '', 
+    email: '', 
+    password:'', 
+    pw_confirm:'', 
+    address:'', 
+    city:'', 
+    state:'', 
+    lucky:'',
+    submitpressed: false
+  };
+
+  message = '';
+
+  pwMatches = () => this.user.password === this.user.pw_confirm;
+
+  allUSStates = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+    'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+    'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+    'Montana Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
+    'Oregon', 'Pennsylvania Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+    'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington'
+  ];
+  
+  popMessageifValid = () => console.log('method called');
+
 }
